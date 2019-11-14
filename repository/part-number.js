a3bb1bb942516f787573e2abe75226cbedc2dad9
bruno-chavez <darklaier@gmail.com>
2019-11-14 18:15:36 -0300

    Repository fetches products from the cache or from the API when necessary

--- repository/part-number.js
+++ repository/part-number.js
@@ -1,5 +1,30 @@
-const partNumber = async function (id) {
-  console.log(id);
+const redisClient = require('../cache/cache');
+const axios = require('axios');
+
+const fetchProduct = async function (id) {
+
+  let product = {};
+
+  redisClient.get(id, (err, data) => {
+    if (err) {
+      console.log(err);
+      return
+    }
+
+    // if there are no matches in the cache, fetch the product from the API
+    if (data === null) {
+      axios.get(`https://simple.ripley.cl/api/v2/products/${id}`).then(p => {
+        product = p;
+
+        // EX 10 sets an expiration time of 10 seconds
+        redisClient.set(id, p, 'EX', 10);
+      });
+    }
+
+    product = data;
+  });
+
+  return product
 };
 
-module.exports = partNumber;
+module.exports = fetchProduct;
